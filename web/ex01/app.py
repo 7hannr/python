7b480ca6f681ab255007ddea5f69cb1460d40939
7hannr <ellen07614@gmail.com>
2024-07-31 13:00:25 +0900

    python

--- web/ex01/app.py
+++ web/ex01/app.py
@@ -1,10 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template
+from routes import bbs
 
 app = Flask(__name__, template_folder="templates")
+app.register_blueprint(bbs.bp)
 
-@app.route('/')
+@app.route("/")
 def index():
-    return "hello......."
+  return render_template("index.html", title="홈페이지", pageName='home.html')
 
-if __name__=='__main__':
-    app.run(port=5000,debug=True)
+
+if __name__ == '__main__':
+  app.run(port=5000, debug=True)
