1b9d7e97fc8c8d3a965e674c1e4fe7bf6da5c532
7hannr <ellen07614@gmail.com>
2024-08-05 18:14:47 +0900

    스크랩핑

---
+++ SCRAP/ex03.py
@@ -0,0 +1,18 @@
+import requests
+from bs4 import BeautifulSoup
+
+#주식인기종목
+url ='https://finance.naver.com/'
+res = requests.get(url)
+
+soup = BeautifulSoup(res.text,'lxml')
+
+e =soup.find('div',attrs={'class':'aside_area aside_popular'}).find('tbody')
+es=e.find_all('tr')
+for e in es:
+    title=e.find('a').get_text()
+    td=e.find_all('td')
+    price=td[0].get_text()
+    updown=td[1].find('span').get_text()
+    updownPrice=td[1].find('span',attrs={'class':'tah p11 nv01'}).get_text().strip()
+    print(title,price,updown,updownPrice)
